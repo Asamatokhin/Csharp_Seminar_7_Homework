10b2336d4b470e054924c6910b07e3a0714cdf8e
Александр <asamatokhin@gmail.com>
2024-02-26 23:25:19 +0300

    add task3 homework

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+// Пример: 
+
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+void Main()
+{
+    int[] myArray = { 4, 3, 2, 7, 12, 8 };
+    PrintArray(myArray);
+
+}
+
+void PrintArray(int[] myArray, int i = 0)
+{
+    if (i < myArray.Length)
+    {
+        PrintArray(myArray, i + 1);
+        System.Console.Write(myArray[i] + " ");
+    }
+}
+
+Main();
